a57e810e014536150935d9ea1d44a9546545d0d9
DejanPopov15 <dejanpopovns@gmail.com>
2026-08-16 12:56:37 +0200

    Working Home and Food navigation

---
+++ app/src/main/java/com/fitmedai/app/FoodActivity.java
@@ -0,0 +1,17 @@
+package com.fitmedai.app;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_food);
+    }
+}
---
+++ app/src/main/java/com/fitmedai/app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.fitmedai.app;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+
+        LinearLayout foodButton = findViewById(R.id.foodButton);
+
+        foodButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
